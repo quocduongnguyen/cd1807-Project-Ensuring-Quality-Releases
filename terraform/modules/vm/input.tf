5ae9e764e886c35afa09658ad6067251d0f0c465
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-11-23 23:52:07 +0700

    Update create vm

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,12 @@
 
+# Resource Group
+variable location {}
+variable "resource_group" {}
+variable "application_type" {}
+
+# Network
+variable virtual_network_name {}
+variable address_prefix_test {}
+variable address_space {}
+variable subnet_id {}
+variable public_ip_address_id {}
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,23 +1,23 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name                = "duongnq9-project3-devops-linux-ni"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = "Standard_DS2_v2"
-  admin_username      = ""
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "duongnq9-project3-devops-linux-vm"
+  location            = var.location
+  resource_group_name = var.resource_group
+  size                = "Standard_B1ls"
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = "adminuser"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCy4maxpA/1EPQQQ2aLgcQIOyEPzPhsieBzGUBCRpGYz5RuBHKxX64chDyMAeVkzf1QfEnGSqR6SeKzTmbYYro7YfjHvgMK8K+eN1WVcwHE9T/7pjQSr5ZFI95PDdzY8UCjHmV6w4Y6Bm6anzmZ+CfTBZwESO1Aj+liL+YBL3cIw5B5FsvUdTPptqyqr/wCxjuHwjMle8Eh+a/oAIjWIRUysrhL1M8dP8cYdpQLaChEmukk1Y6eWcDPROgOOm/HFapnq4rayvu4g5EPDxv64VY7Oun1lZ8lt0X6y0vVMYSVv3PpKw55v6725tUB31Sw8r+so/yyKgevdgzHzQYV64pHAoFFFKwHyIDC1EzNAuyKQedrR0aEvZkNQy1xaXIlF/UT6s/QPDNEQB+65wA0oSmKPqfrn+FNwZ/niLeQTHz6Sxc6bNX4HSJt4TLBaoTMrM9tDjmUww+KX0YQzdJbedAuvToQvimEZALMiwAyj5xNP14o6d/Kb21SlTZQTRZzxXs= quocd@QuocDuong"
