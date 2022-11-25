70abb05b91d71d6ac1b0e6f9d47df1651f2ce003
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-11-25 17:17:50 +0700

    Move fakerestapi

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 }
