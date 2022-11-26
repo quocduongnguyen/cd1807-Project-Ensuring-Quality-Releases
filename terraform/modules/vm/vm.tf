c65f67f7e39ec49375e9176464da29f01aa843a2
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-11-26 15:50:55 +0700

    Update sku

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 }
